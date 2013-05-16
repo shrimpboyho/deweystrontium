83104d202a69accbf5f364e850a6c37f26843b59
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-05-16 16:24:48 -0400

    Added initial files

---
+++ DeweyStrontium/DeweyStrontium/main.cpp
@@ -0,0 +1,17 @@
+// Include headers
+
+#include <SFML\Graphics.hpp>
+#include <iostream>
+
+// Main function
+
+int main(){
+
+	// Create window
+	sf::RenderWindow window(sf::VideoMode(800,600),"Dewey Strontium");
+	
+	
+	// Keep console open
+	std::cin.get();
+	return 0;
+}
